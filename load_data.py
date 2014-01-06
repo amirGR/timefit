2db211ace2f29f60f792c87301c60fe9ccc85cdb
Ronnie Maor <ronnie.maor@gmail.com>
2014-01-06 12:18:38 +0200

    minor (rename)

--- load_data.py
+++ load_data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Data(object): pass
     
-def cell_array_to_list_of_string(cell_array):
+def convert_matlab_string_cell(cell_array):
     return np.array([x[0] for x in cell_array.flat])
 
 def load_data():
@@ -13,8 +13,8 @@ def load_data():
     mat = loadmat(path)
     data = Data()
     data.expression = mat['expression']
-    data.gene_names = cell_array_to_list_of_string(mat['gene_names'])
-    data.region_names = cell_array_to_list_of_string(mat['region_names'])
-    data.genders = cell_array_to_list_of_string(mat['genders'])
+    data.gene_names = convert_matlab_string_cell(mat['gene_names'])
+    data.region_names = convert_matlab_string_cell(mat['region_names'])
+    data.genders = convert_matlab_string_cell(mat['genders'])
     data.ages = mat['ages'][0,:]
     return data
