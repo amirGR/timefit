2f29cc3e7d80c2cefbe681d5b0a63944239cb797
Ronnie Maor <ronnie.maor@gmail.com>
2014-01-08 17:15:13 +0200

    fits - first version with no gradient

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from load_data import load_data
 import numpy as np
 import scipy as sp
+from scipy import optimize
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -20,3 +21,35 @@ def plot_gene(iGene):
             ax.set_xlabel('Age [years]')
     fig.tight_layout(h_pad=0,w_pad=0)
     fig.suptitle('Gene {}'.format(series.gene_name))
+    
+def plot_one_fit(series,theta=None):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(series.ages,series.expression,'ro')
+    ax.plot(series.ages,series.expression,'b-')
+    if theta is not None:
+        fit = sigmoid(theta,series.ages)
+        ax.plot(series.ages,fit,'g-')
+    ax.set_title('Region {}'.format(series.gene_name))
+    ax.set_ylabel('Expression Level')
+    ax.set_xlabel('Age [years]')
+    
+def sigmoid(theta,x):
+    a,h,mu,w = theta
+    return a + h/(1+np.exp(-(x-mu)/w))
+
+def f_error(theta,x,y):
+    squares = (sigmoid(theta,x) - y)**2
+    return 0.5*sum(squares)
+
+def fit_sigmoid(series):
+    expr = series.expression
+    ages = series.ages
+    theta0 = np.array([
+        expr.min(), # a
+        expr.max(), # h
+        (ages.min() + ages.max()) / 2, # mu
+        (ages.max() - ages.min()) / 2, # w
+    ])
+    res = optimize.minimize(f_error, theta0, args=(ages,expr), method='Nelder-Mead')
+    return res.x
