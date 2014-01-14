387054225ba2c740f2f5df388f15954a881cef8f
Ronnie Maor <ronnie.maor@gmail.com>
2014-01-14 20:49:48 +0200

    minor

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ n_optimization_attempts = 5
 theta_prior = np.array([5, 5, 30, 2.5])
 theta_prior_sigma = np.array([5, 5, 30, 2.5])
 
+# these two settings must change together
+from sklearn.metrics import r2_score as score
 score_type = 'R2'
-from sklearn.metrics import r2_score, explained_variance_score
-dct_scores = {'R2' : r2_score, 'Explained Variance' : explained_variance_score}
-score = dct_scores[score_type]
