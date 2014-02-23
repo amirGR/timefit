92c59b7a6cf956f4fdc838af6367d020266057ef
Ronnie Maor <ronnie.maor@gmail.com>
2014-02-23 17:06:55 +0200

    method comparison

--- scripts/compare_fit_approaches.py
+++ scripts/compare_fit_approaches.py
@@ -1,34 +1,38 @@
 import setup
+import numpy as np
+import matplotlib.pyplot as plt
+import config as cfg
 from load_data import load_data
-import sigmoid_fit as new_fits
-import sigmoid_fit_hadas as old_fits
-from fit_score import loo_score
+from all_fits import get_all_fits
+from scipy.stats import spearmanr, wilcoxon
 
-def compare(gene,region):
-    data = load_data()   
-    series = data.get_one_series(gene,region)
-    x = series.ages
-    y = series.expression
-    
-    print 'Fitting using old method...'
-    old_preds = old_fits.fit_sigmoid_loo(x,y)
-    
-    print 'Fitting using new method...'
-    new_preds = new_fits.fit_sigmoid_loo(x,y)
-    
-    print 'Comparing...'
-    old_score = loo_score(y,old_preds)
-    new_score = loo_score(y,new_preds)
-    print 'Old score = {}'.format(old_score)
-    print 'New score = {}'.format(new_score)
+import utils
+utils.disable_all_warnings()
 
-import sys
-if __name__ == '__main__':
-    try:
-        progname, gene, region = sys.argv
-    except:
-        progname = sys.argv[0]
-        print 'Usage: {} <gene> <region>'.format(progname)
-    else:
-        compare(gene,region)
+data = load_data('serotonin', 'kang2011')
+fits = get_all_fits(data, b_hadas_fits=False)
+fits_hadas = get_all_fits(data, b_hadas_fits=True)
 
+matched_scores = [(fits_hadas[k].LOO_score,fits[k].LOO_score) for k in fits.iterkeys()]
+old_scores = np.array([old for old,new in matched_scores])
+new_scores = np.array([new for old,new in matched_scores])
+score_diff = new_scores - old_scores
+
+r, p_value = spearmanr(old_scores, new_scores)
+T, signed_rank_p_value = wilcoxon(old_scores, new_scores)
+
+# as histogram
+fig = plt.figure()
+plt.hist(score_diff, range=(-1,1), bins=20)
+plt.title('Comparison of fit methods on held out data\nWilcoxon signed rank p-value={:.4f}'.format(signed_rank_p_value), fontsize=cfg.fontsize)
+plt.xlabel(r'R2(global MLE) - R2($\lambda$ by CV)', fontsize=cfg.fontsize)
+plt.ylabel(r'Number of gene/regions', fontsize=cfg.fontsize)
+
+# as scatter plot
+fig = plt.figure()
+plt.scatter(old_scores, new_scores, alpha=0.5)
+plt.plot(np.linspace(-1,1,100),np.linspace(-1,1,100),'b--')
+plt.axis([-1,1,-1,1])
+plt.title('Comparison of fit methods on held out data\n(Spearman r={:.2f})'.format(r), fontsize=cfg.fontsize)
+plt.xlabel(r'R2($\lambda$ by CV)', fontsize=cfg.fontsize)
+plt.ylabel(r'R2(global MLE)', fontsize=cfg.fontsize)
