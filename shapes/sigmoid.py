fixed numerical instability in computation of sigmoid gradient

shapes/sigmoid.py
@@ -20,10 +20,11 @@ class Sigmoid(Shape):
     def f_grad(self,theta,x):
         a,h,mu,w = theta
         e = np.exp(-(x-mu)/w)
+        ie = np.exp((x-mu)/w)
         d_a = 1
         d_h = 1/(1+e)
-        d_mu = -h/(1+e)**2 * e/w
-        d_w = -h/(1+e)**2 * e * (x-mu)/w**2
+        d_mu = -h/(w*(1+e)*(1+ie))
+        d_w = -h*(x-mu)/(w**2 * (1+e) * (1+ie))
         return [d_a, d_h, d_mu, d_w]
     
     def get_theta_guess(self,x,y):