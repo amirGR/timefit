6ae4f48ddb5553c137753afbb31902128692f8c9
Ronnie Maor <ronnie.maor@gmail.com>
2014-03-07 08:45:17 +0200

    minor

--- scripts/create_html.py
+++ scripts/create_html.py
@@ -4,9 +4,11 @@ from load_data import load_data
 from plots import save_fits_and_create_html
 from fitter import Fitter
 from shapes.sigmoid import Sigmoid
+from shapes.poly import Poly
 
 data = load_data()
-for theta_prior in [False,True]:
-    fitter = Fitter(Sigmoid(),theta_prior,False)
-    basedir = join(r'C:\temp\HTR', 'theta-{}'.format(theta_prior)) 
+for n in [1,2,3]:
+    shape = Poly(n)
+    fitter = Fitter(shape,False,False)
+    basedir = join(r'C:\temp\HTR', shape.cache_name()) 
     save_fits_and_create_html(data, fitter, basedir)
