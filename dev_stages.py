497d798c35cbaa7e82c8d0066f6224db64aa9e41
Ronnie Maor <ronnie.maor@gmail.com>
2014-04-03 11:36:52 +0300

    added some code to help map ages to stages

--- dev_stages.py
+++ dev_stages.py
@@ -28,7 +28,8 @@ def M(x):
     return x / 12.0
 
 class DevStage(object):
-    def __init__(self, name, short_name, from_age, to_age):
+    def __init__(self, num, name, short_name, from_age, to_age):
+        self.num = num
         self.name = name
         self.short_name = short_name
         self.from_age = float(from_age)
@@ -42,6 +43,7 @@ class DevStage(object):
         import scalers
         scaler = scalers.unify(scaler) # handle None
         return DevStage(
+            num = self.num,
             name = self.name,
             short_name = self.short_name,
             from_age = scaler.scale(self.from_age),
@@ -49,19 +51,26 @@ class DevStage(object):
         )
 
 dev_stages = [
-    DevStage('Embryonic', 'E1', PCW(4), PCW(8)),
-    DevStage('Early fetal', 'EF2', PCW(8), PCW(10)),
-    DevStage('Early fetal', 'EF3', PCW(10), PCW(13)),
-    DevStage('Early mid-fetal', 'EMF4', PCW(13), PCW(16)),
-    DevStage('Early mid-fetal', 'EMF5', PCW(16), PCW(19)),
-    DevStage('Late mid-fetal', 'LMF6', PCW(19), PCW(24)),
-    DevStage('Late fetal', 'LF7', PCW(24), PCW(38)),
-    DevStage('Neonatal and early infancy', 'EI8', M(0), M(6)),
-    DevStage('Late infancy', 'LI9', M(6), M(12)),
-    DevStage('Early childhood', 'EC10', 1, 6),
-    DevStage('Middle and late childhood', 'MLC11', 6, 12),
-    DevStage('Adolescence', 'Adol12', 12, 20),
-    DevStage('Young adulthood', 'YA13', 20, 40),
-    DevStage('Middle adulthood', 'MA14', 40, 60),
-    DevStage('Late adulthood', 'LA15', 60, 80),
+    DevStage(1,'Embryonic', 'E1', PCW(4), PCW(8)),
+    DevStage(2,'Early fetal', 'EF2', PCW(8), PCW(10)),
+    DevStage(3,'Early fetal', 'EF3', PCW(10), PCW(13)),
+    DevStage(4,'Early mid-fetal', 'EMF4', PCW(13), PCW(16)),
+    DevStage(5,'Early mid-fetal', 'EMF5', PCW(16), PCW(19)),
+    DevStage(6,'Late mid-fetal', 'LMF6', PCW(19), PCW(24)),
+    DevStage(7,'Late fetal', 'LF7', PCW(24), PCW(38)),
+    DevStage(8,'Neonatal and early infancy', 'EI8', M(0), M(6)),
+    DevStage(9,'Late infancy', 'LI9', M(6), M(12)),
+    DevStage(10,'Early childhood', 'EC10', 1, 6),
+    DevStage(11,'Middle and late childhood', 'MLC11', 6, 12),
+    DevStage(12,'Adolescence', 'Adol12', 12, 20),
+    DevStage(13,'Young adulthood', 'YA13', 20, 40),
+    DevStage(14,'Middle adulthood', 'MA14', 40, 60),
+    DevStage(15,'Late adulthood', 'LA15', 60, 80),
 ]
+
+def map_age_to_stage(age):
+    for i,stage in enumerate(dev_stages):
+        if age < stage.to_age:
+            return stage
+    else:
+        return dev_stages[-1]
