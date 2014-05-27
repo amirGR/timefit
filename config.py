0447242a51c730cd7206b4d292f037d952480c98
Ronnie Maor <ronnie.maor@gmail.com>
2014-05-27 15:57:27 +0300

    added dopamine pathway

--- config.py
+++ config.py
@@ -63,6 +63,23 @@ pathways = {
         'CNR1','CNR2','CALB1','CALB2','DAGLA','DAGLB',
         'NAPEPLD','GDE1','ABHD4','PTPN22', 'MGLL','ABHD6','FAAH',
     ],
+    'dopamine' : [
+        'ADCY5', 'AKT1', 'AKT2', 'AKT3', 'ARNTL', 'ARRB2', 'ATF2', 'ATF4', 'ATF6B', 
+        'CACNA1A', 'CACNA1B', 'CACNA1C', 'CACNA1D', 'CALM1', 'CALML3', 'CALML5', 'CALML6', 
+        'CALY', 'CAMK2A', 'CAMK2B', 'CAMK2D', 'CAMK2G', 'CLOCK', 'COMT', 'CREB1', 'CREB3', 
+        'CREB3L1', 'CREB3L2', 'CREB3L3', 'CREB3L4', 'CREB5', 'DDC', 
+        'DRD1', 'DRD2', 'DRD3', 'DRD4', 'DRD5', 'FOS', 'GNAI1', 'GNAI2', 'GNAI3', 'GNAL', 'GNAO1', 
+        'GNAQ', 'GNAS', 'GNB1', 'GNB2', 'GNB3', 'GNB4', 'GNB5', 'GNG10', 'GNG11', 'GNG12', 'GNG13', 
+        'GNG2', 'GNG3', 'GNG4', 'GNG5', 'GNG7', 'GNG8', 'GNGT1', 'GNGT2', 
+        'GRIA1', 'GRIA2', 'GRIA3', 'GRIA4', 'GRIN2A', 'GRIN2B', 'GSK3A', 'GSK3B', 'ITPR1', 'ITPR2', 'ITPR3', 
+        'KCNJ3', 'KCNJ5', 'KCNJ6', 'KCNJ9', 'KIF5A', 'KIF5B', 'KIF5C', 'MAOA', 'MAOB', 
+        'MAPK10', 'MAPK11', 'MAPK12', 'MAPK13', 'MAPK14', 'MAPK8', 'MAPK9', 
+        'PLCB1', 'PLCB2', 'PLCB3', 'PLCB4', 'PPP1CA', 'PPP1CB', 'PPP1CC', 'PPP1R1B', 'PPP2CA', 
+        'PPP2CB', 'PPP2R1A', 'PPP2R1B', 'PPP2R2A', 'PPP2R2B', 'PPP2R2C', 'PPP2R2D', 'PPP2R3A', 
+        'PPP2R3B', 'PPP2R3C', 'PPP2R5A', 'PPP2R5B', 'PPP2R5C', 'PPP2R5D', 'PPP2R5E', 'PPP3CA', 
+        'PPP3CB', 'PPP3CC', 'PRKACA', 'PRKACB', 'PRKACG', 'PRKCA', 'PRKCB', 'PRKCG', 'PRKX', 
+        'SCN1A', 'SLC18A1', 'SLC18A2', 'SLC6A3', 'TH'
+    ],
     '17pathways': '17pathways.txt', # references a file you should hopefully have in the data directory
     '17full': '17pathways-full.txt', # references a file you should hopefully have in the data directory
 }
