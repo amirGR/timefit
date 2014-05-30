04bef00a5a37b640bbdaedae9d7a44947b0eefab
Ronnie Maor <ronnie.maor@gmail.com>
2014-05-30 09:23:25 +0300

    added --from_age command line option

--- command_line.py
+++ command_line.py
@@ -5,6 +5,7 @@ from scalers import allowed_scaler_names, build_scaler
 from shapes.shape import get_shape_by_name, allowed_shape_names
 from shapes.priors import get_allowed_priors
 from fitter import Fitter
+from dev_stages import PCW
 
 class MyParser(argparse.ArgumentParser):  
     def convert_arg_line_to_args(self, arg_line):
@@ -15,6 +16,11 @@ class MyParser(argparse.ArgumentParser):
                 continue
             yield arg
 
+dct_ages = {
+    'postnatal' : 0,
+    'EF3' : PCW(10),
+}
+
 def get_common_parser(include_pathway=True):
     parser = MyParser(fromfile_prefix_chars='@')
     group = parser.add_mutually_exclusive_group()
@@ -23,7 +29,7 @@ def get_common_parser(include_pathway=True):
     parser.add_argument('--dataset', default='both', help='Default: both', choices=['kang2011', 'colantuoni2011', 'both'])  
     if include_pathway:
         parser.add_argument('--pathway', default='serotonin', help='Default: serotonin', choices=['all'] + cfg.pathways.keys())
-    parser.add_argument('--postnatal', help='Use only postnatal data points', action='store_true')
+    parser.add_argument('--from_age', help='Use only data points with larger ages than this. Default: all ages', choices=dct_ages.keys())
     parser.add_argument('--scaling', help='What scaling to use for ages. Default: none', choices=allowed_scaler_names())
     parser.add_argument('--shuffle', help='Shuffle the y-values of the data', action='store_true')
     parser.add_argument('-s', '--shape', help='The shape to use for fitting. Default: sigmoid', default='sigmoid', choices=allowed_shape_names())
@@ -34,12 +40,16 @@ def get_common_parser(include_pathway=True):
 def process_common_inputs(args):
     cfg.verbosity = args.verbose - args.quiet
     pathway = getattr(args, 'pathway', None)
-    data = get_data_from_args(args.dataset, pathway, args.postnatal, args.scaling, args.shuffle)
+    data = get_data_from_args(args.dataset, pathway, args.from_age, args.scaling, args.shuffle)
     fitter = get_fitter_from_args(args.shape, args.priors, args.sigma_prior)
     return data,fitter
 
-def get_data_from_args(dataset, pathway, postnatal, scaling, shuffle):
-    data = GeneData.load(dataset).restrict_pathway(pathway).restrict_postnatal(postnatal)
+def get_data_from_args(dataset, pathway, from_age, scaling, shuffle):
+    data = GeneData.load(dataset).restrict_pathway(pathway)
+    if from_age is not None:
+        restriction_name = from_age
+        from_age = dct_ages[from_age]
+        data.restrict_ages(restriction_name,from_age=from_age)
     if scaling is not None:
         scaler = build_scaler(scaling,data)
         data.scale_ages(scaler)
