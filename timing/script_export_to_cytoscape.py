e4115c7f9e7c3f2a9e27c05698abcfc7b6c78c5e
Ronnie Maor <ronnie.maor@gmail.com>
2014-10-09 09:23:53 +0300

    added pval_cutoff

--- timing/script_export_to_cytoscape.py
+++ timing/script_export_to_cytoscape.py
@@ -16,13 +16,15 @@ def save_file(filename, lines):
     with open(filename,'w') as f:
         f.write(txt)
 
-def export_cytoscape(timing):
+def export_cytoscape(timing, pval_cutoff):
     res = timing.analyze_all_pathways().filter_regions(exclude=['PFC'])
     def safe_pathway_name(pathway): return re.sub(r'\s+','-',pathway)
     def edge_weight(pval): return min(200, int(-50/np.log10(pval)))
-    lines = ['{} {} {}'.format(x.r1, safe_pathway_name(x.pathway), x.r2) for x in res.res]
+    vals = [(x.r1, safe_pathway_name(x.pathway), x.r2, edge_weight(x.pval)) for x in res.res  if -np.log10(x.pval) > pval_cutoff]
+    
+    lines = ['{} {} {}'.format(r1,pathway,r2) for r1,pathway,r2,w in vals]
     save_file(join(results_dir(), 'cytoscape', 'regions.sif'), lines)
-    lines = ['{} ({}) {} = {}'.format(x.r1, safe_pathway_name(x.pathway), x.r2, edge_weight(x.pval)) for x in res.res]
+    lines = ['{} ({}) {} = {}'.format(r1, pathway, r2, w) for r1, pathway, r2, w in vals]
     save_file(join(results_dir(), 'cytoscape', 'edge_weights.attrs'), ['EdgeWeights'] + lines)
 
 ##############################################################
@@ -31,6 +33,8 @@ def export_cytoscape(timing):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--list', help='Pathways list name. Default=brain_go_num_genes_min_15', default='brain_go_num_genes_min_15', choices=['all'] + pathway_lists.all_pathway_lists())
+    parser.add_argument('--pval_cutoff', help='Only write edges where the -log(p-value) is above this threshold. Default=0 (use all edges)', default='0')
     args = parser.parse_args()
+    pval_cutoff = float(args.pval_cutoff)
     timing = RegionPairTiming(args.list)
-    export_cytoscape(timing)
+    export_cytoscape(timing, pval_cutoff)
