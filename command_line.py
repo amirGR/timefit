3e1495afa47702008b71e138de1f39a5da22c5f5
amirGR <amirgraitzer@gmail.com>
2015-01-31 18:12:12 +0200

    add RNA-Seq to known datasets, set config keys
    - add RNA-Seq datasets to known datasets
    - add new command line argument - plots_scaling

--- command_line.py
+++ command_line.py
@@ -21,7 +21,8 @@ dct_ages = {
     'EF3' : PCW(10),
 }
 
-known_datasets = ['kang2011', 'colantuoni2011', 'both', 'harris2009', 'somel2010-human', 'brainspan2014']
+known_datasets = ['kang2011', 'colantuoni2011', 'both', 'harris2009', 'somel2010-human', 'brainSpan2014', 
+                  'brainspan2014exons','brainspan2014exons17pathways']
 
 def get_common_parser(include_pathway=True):
     parser = MyParser(fromfile_prefix_chars='@')
@@ -37,10 +38,13 @@ def get_common_parser(include_pathway=True):
     parser.add_argument('-s', '--shape', help='The shape to use for fitting. Default: sigslope', default='sigslope', choices=allowed_shape_names())
     parser.add_argument('--sigma_prior', help='Prior to use for 1/sigma when fitting. Default: normal', default = 'normal', choices=get_allowed_priors(is_sigma=True))
     parser.add_argument('--priors', help='Priors to use for theta when fitting. Default: sigslope80', default = 'sigslope80', choices=get_allowed_priors())
+    parser.add_argument('--plots_scaling', help = 'scaling of expression level (y axis) on exons plots . default: log', default = 'log', choices=allowed_scaler_names())
     return parser
 
 def process_common_inputs(args):
     cfg.verbosity = args.verbose - args.quiet
+    cfg.plots_scaling =  args.plots_scaling
+    cfg.exon_level = args.dataset in ['brainspan2014exons','brainspan2014exons17pathways']
     pathway = getattr(args, 'pathway', None)
     data = get_data_from_args(args.dataset, pathway, args.from_age, args.scaling, args.shuffle)
     fitter = get_fitter_from_args(args.shape, args.priors, args.sigma_prior)
